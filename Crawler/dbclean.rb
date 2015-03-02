b439deceab8c9bc105d3eeaf406899ebaef71410
gagagithub <firefox.cui@gmail.com>
2015-03-02 23:15:00 +0800

    database backup 2

--- Crawler/dbclean.rb
+++ Crawler/dbclean.rb
@@ -7,14 +7,14 @@ begin
     
     con = Mysql.new 'localhost', 'root', 'zhongren#1234', 'kuailv-production'    
 
-    con.query "CREATE TABLE activitiesbackup \ 
-    			SELECT * FROM activities"
-
-
-#    con.query "INSERT IGNORE INTO activitiesbackup \ 
+#    con.query "CREATE TABLE activitiesbackup \ 
 #    			SELECT * FROM activities"
 
-#    con.query "DELETE FROM activities WHERE start_time < curdate()"
+
+    con.query "INSERT IGNORE INTO activitiesbackup \ 
+    			SELECT * FROM activities"
+
+    con.query "DELETE FROM activities WHERE start_time < curdate()"
 
 
         
