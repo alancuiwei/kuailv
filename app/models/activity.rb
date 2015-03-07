36083f73199e616a34803b7cafca4d4b0dbdb725
gagagithub <firefox.cui@gmail.com>
2015-03-07 19:35:57 +0800

    multi-beauty input 3

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,12 +3,12 @@ class Activity < ActiveRecord::Base
 	mount_uploader :avatar, AvatarUploader
 	
 	validates :qq, 
-	:uniqueness => true
+	:uniqueness => true,
 	:allow_nil => true,
 	:allow_blank => true	
 
 	validates :f_wechatid, 
-	:uniqueness => true
+	:uniqueness => true,
 	:allow_nil => true,
 	:allow_blank => true	
 end
