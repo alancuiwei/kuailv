fe030da5f20c6b51427768db7bce0e9de9095cc0
gagagithub <firefox.cui@gmail.com>
2015-03-10 17:50:20 +0800

    西藏活动 9

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -99,8 +99,8 @@ class WeixinsController < ApplicationController
               render "rtn404", :format => :xml
             else
  
-              if (@resultactivities.count > 9)
-                @resultactivities = @resultactivities.first(9)
+              if (@resultactivities.count > 10)
+                @resultactivities = @resultactivities.first(10)
               end
               @messagenum = @resultactivities.count
               render "rtn130", :formats => :xml
