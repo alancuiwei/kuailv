340fea82fef366f37dbf3de8d349c19157ede1a6
gagagithub <firefox.cui@gmail.com>
2015-03-18 13:27:51 +0800

    南京游客线下导流 13

--- Crawler/dbclean.rb
+++ Crawler/dbclean.rb
@@ -7,14 +7,11 @@ begin
     
     con = Mysql.new 'localhost', 'root', 'zhongren#1234', 'kuailv-production'    
 
-#    con.query "CREATE TABLE activitiesbackup \ 
-#    			SELECT * FROM activities"
-
-
     con.query "INSERT IGNORE INTO activitiesbackup \ 
     			SELECT * FROM activities"
 
-    con.query "DELETE FROM activities WHERE start_time < curdate()"
+#    con.query "DELETE FROM activities WHERE start_time < curdate()"
+    con.query "DELETE FROM activities WHERE start_time < '2015-01-01'"
 
 
         
